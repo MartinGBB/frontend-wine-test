ad26a372440bcceaf230cbe9e0a0c85e8a38bcc3
Martin <escorpmartin97@gmail.com>
2022-06-30 21:30:57 -0300

    style: identa slidebar e ajusta para telas mais pequenas

--- src/components/Slidebar/styles.ts
+++ src/components/Slidebar/styles.ts
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
 export const ContainerSlidebar = styled.div`
+  @media (max-width: 800px) {
+    visibility: hidden;
+    width: 0;
+    margin: 0;
+  }
+
   display: flex;
   flex-direction: column;
   height: 100vh;
-
+  width: 28rem;
 `;

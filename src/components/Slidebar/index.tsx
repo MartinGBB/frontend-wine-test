871707d0dbb3554fede38383c6bf525fe5df059d
Martin <escorpmartin97@gmail.com>
2022-06-30 21:44:58 -0300

    feat: adiciona lista de precos

--- src/components/Slidebar/index.tsx
+++ src/components/Slidebar/index.tsx
@@ -3,7 +3,13 @@ import { ContainerSlidebar } from './styles';
 export function Slidebar() {
   return (
     <ContainerSlidebar>
-      <h1>Slidebar</h1>
+      <ul>
+        <li><input type="radio" />Até R$40</li>
+        <li><input type="radio" />R$40 A R$60</li>
+        <li><input type="radio" />R$100 A R$200</li>
+        <li><input type="radio" />R$200 A R$500</li>
+        <li><input type="radio" />Acima de R$500</li>
+      </ul>
     </ContainerSlidebar>
   )
 };

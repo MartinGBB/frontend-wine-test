7428a54a14f856c6443263268aa624d547f42665
Martin <escorpmartin97@gmail.com>
2022-07-02 00:50:14 -0300

    feat: cria funcao que chama dados da api e chama dados no componente de cards

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,24 +1,41 @@
+import { useEffect } from "react";
+import { fetchApi } from "../../utils/fetchApi";
 import { formatPrice } from "../../utils/formatDataApi";
 import { Cards } from "../Cards";
 import { Container, ContainerCardList } from "./styles";
 
 export function CardList() {
+  const handleFetch = async () => {
+    const data = await fetchApi()
+    return data;
+  };
 
+  useEffect(() => {
+    handleFetch();   
+}, []);
+
+  console.log(data)
   const newFormatPrice = formatPrice(390.9);
 
   return (
     <Container>
       <p>{ 49 } produtos encontrados</p>
     <ContainerCardList>
-      <Cards 
-        image="https://www.wine.com.br/cdn-cgi/image/f=png,h=515,q=99/assets-images/produtos/26453-01.png"
-        name={ 'vinho' }
-        price={ newFormatPrice }
-        discount={ 50 }
-        priceMemberInt={ 100 }
-        priceMemberPennies={ 99 }
-        priceNonMember={ newFormatPrice }
-      />
+      {
+        data.map((item) => ( 
+          <Cards 
+          key={ item.id }
+          image={ item }
+          name={ item }
+          price={ item }
+          discount={ item }
+          priceMemberInt={ item }
+          priceMemberPennies={ item }
+          priceNonMember={ item }
+          />
+          )
+        )
+      };
     </ContainerCardList>
     </Container>
   )

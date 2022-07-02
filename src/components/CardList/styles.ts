a5663afb5e0d7f1fcccee9488487bdf1be112596
Martin <escorpmartin97@gmail.com>
2022-07-01 21:01:03 -0300

    style: corrige margem do paragrafo e o card

--- src/components/CardList/styles.ts
+++ src/components/CardList/styles.ts
@@ -7,6 +7,6 @@ export const ContainerCardList = styled.div`
     color: #262626;
     margin-top: 0.711rem;
     margin-bottom: 2rem;
-    margin: 0.711rem 0 2rem 0.7rem;
+    margin: 0.711rem 0 1rem 0.7rem;
   }
 `;

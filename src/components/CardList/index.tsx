4f648ca8d1d9bcd688203deda99897d0e309520e
Martin <escorpmartin97@gmail.com>
2022-07-02 01:10:59 -0300

    feat: muestra em tela dados da api

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,40 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchApi } from "../../utils/fetchApi";
-import { formatPrice } from "../../utils/formatDataApi";
+// import { formatPrice } from "../../utils/formatDataApi";
 import { Cards } from "../Cards";
 import { Container, ContainerCardList } from "./styles";
 
 export function CardList() {
+  const [products, setProducts] = useState(['']);
+
   const handleFetch = async () => {
-    const data = await fetchApi()
-    return data;
+    const data = await fetchApi();
+    setProducts(data);
   };
 
   useEffect(() => {
-    handleFetch();   
+    handleFetch(); 
 }, []);
 
-  console.log(data)
-  const newFormatPrice = formatPrice(390.9);
+  // const newFormatPrice = formatPrice(390.9);
 
   return (
     <Container>
       <p>{ 49 } produtos encontrados</p>
     <ContainerCardList>
       {
-        data.map((item) => ( 
-          <Cards 
-          key={ item.id }
-          image={ item }
-          name={ item }
-          price={ item }
-          discount={ item }
-          priceMemberInt={ item }
-          priceMemberPennies={ item }
-          priceNonMember={ item }
-          />
+        products.map((product) => {
+          return (
+            <Cards 
+              key={ product.id }
+              image={ product.image }
+              name={ product.name }
+              price={ product.price }
+              discount={ product.discount }
+              priceMemberInt={ product.priceMember }
+              priceMemberPennies={ product.priceMember }
+              priceNonMember={ product.priceNonMember }
+            />
           )
-        )
+          })
       };
     </ContainerCardList>
     </Container>

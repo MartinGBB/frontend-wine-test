a2a8991dcbe253b66fd68dee990155c9f5d2d4bc
Martin <escorpmartin97@gmail.com>
2022-07-02 01:35:14 -0300

    feat: adiciona estado para controlar quantidade de items exibidos

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import { fetchApi } from "../../utils/fetchApi";
-// import { formatPrice } from "../../utils/formatDataApi";
+import { formatPrice } from "../../utils/formatDataApi";
 import { Cards } from "../Cards";
 import { Container, ContainerCardList } from "./styles";
 
 export function CardList() {
-  const [products, setProducts] = useState(['']);
+  const [products, setProducts] = useState([]);
+  const [quantityProducts, setQuantityProducts] = useState('');
 
   const handleFetch = async () => {
     const data = await fetchApi();
-    setProducts(data);
+    setQuantityProducts(data.totalItems);
+    setProducts(data.items);
   };
 
   useEffect(() => {
@@ -20,7 +22,7 @@ export function CardList() {
 
   return (
     <Container>
-      <p>{ 49 } produtos encontrados</p>
+      <p>{ quantityProducts } produtos encontrados</p>
     <ContainerCardList>
       {
         products.map((product) => {
@@ -29,15 +31,15 @@ export function CardList() {
               key={ product.id }
               image={ product.image }
               name={ product.name }
-              price={ product.price }
+              price={ formatPrice(product.price) }
               discount={ product.discount }
               priceMemberInt={ product.priceMember }
               priceMemberPennies={ product.priceMember }
-              priceNonMember={ product.priceNonMember }
+              priceNonMember={ formatPrice(product.priceNonMember) }
             />
           )
           })
-      };
+      }
     </ContainerCardList>
     </Container>
   )
--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -2,7 +2,8 @@ export const fetchApi = async () => {
   try {
     const endpoint = `https://wine-back-test.herokuapp.com/products?page=1&limit=9`
     const data = await (await fetch(endpoint)).json();
-    return data.items;
+    console.log(data)
+    return data;
   } catch ({ message }) {
     alert(message)
   };

8ddedefb91c4fde16854de8a75991b79756c193a
Martin <escorpmartin97@gmail.com>
2022-07-03 13:58:20 -0300

    fix: coloca parentesis faltante

--- src/utils/searchPrice.ts
+++ src/utils/searchPrice.ts
@@ -7,7 +7,7 @@ export const searchPrice = (filterPrice: string | undefined, pricesApi: string[]
       return pricesApi?.filter(({ price }: number | any) => Number(price) >= 40 && Number(price) <= 60);
 
     case "100-200":
-      return pricesApi?.filter(({ price }): number | any => Number(price) >= 100 && Number(price) <= 200);
+      return pricesApi?.filter(({ price }: number | any) => Number(price) >= 100 && Number(price) <= 200);
 
     case "200-500":
       return pricesApi?.filter(({ price }: number | any) => Number(price) >= 200 && Number(price) <= 500);

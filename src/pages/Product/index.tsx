97e73c63fc6663dfabf818a1d334c9d4ac323a4d
Martin <escorpmartin97@gmail.com>
2022-07-03 15:59:56 -0300

    feat: configura componente

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,7 +1,13 @@
+import { Header } from "../../components/Header";
+import { ContainerProduct } from "./styles";
+
 export function Product() {
   return (
     <div>
-      <h1>Product</h1>
+      <Header />
+      <ContainerProduct>
+        
+      </ContainerProduct>
     </div>
   )
 }
